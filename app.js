3281dfb8d594060c9c8a5ebd76160c28974792d0
Sumit-531 <sumitchakraborty218@gmail.com>
2025-11-13 01:34:11 +0600

    add MongoDB connection setup using Mongoose

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import { PORT } from "./config/env.js";
 import express from "express";
 import authRouter from "./routes/auth.routes.js";
 import userRouter from "./routes/user.routes.js";
+import connectToDatabase from "./database/mongodb.js";
 
 const app = express();
 
@@ -10,7 +11,9 @@ app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
 
 
-app.listen(PORT, () =>{
+app.listen(PORT, async () =>{
     console.log(`LinkUp is running on http://localhost:${PORT}`);
+
+    await connectToDatabase();
 });
 
--- config/env.js
+++ config/env.js
@@ -5,4 +5,6 @@ config({ path: `.env.${process.env.NODE_ENV || "development"}.local` });
 
 export const {
     PORT,
+    NODE_ENV,
+    DB_URI,
 } = process.env;
